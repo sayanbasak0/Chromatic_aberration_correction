de0fca625d4620bb9edecc421a3c700e3a153efe
sayanbasak0 <77215746+sayanbasak0@users.noreply.github.com>
2023-05-29 22:51:34 -0700

    adding ctests

--- main.cpp
+++ main.cpp
@@ -6,26 +6,38 @@
 #include "ColorAberrationCorrection.h"
 
 int main(int argc, char *argv[]) {
+	int is_interactive = 0;
 	if (argc<2) {
 		std::cout << "No input file(s) !" << std::endl;
 		return -1;
 	}
+	
 	for (int i=1; i<argc; i++){
 		std::string inputfile = std::string(argv[i]);
+		if (inputfile=="-i"){
+			is_interactive = 1;
+			continue;
+		}
+		else if (inputfile=="-b"){
+			is_interactive = 0;
+			continue;
+		}
+		
 		std::string outputfile = inputfile+"_CAC.bmp";
 		cv::Mat src = cv::imread(inputfile, cv::IMREAD_COLOR);
 		
 		cv::Mat dst;
 
 		CAC::CACorrection(src, dst);
-
-		cv::imshow(inputfile+" [original]", src);
-		cv::imshow(outputfile+" [result]", dst);
-		
-		cv::imwrite(outputfile, dst);
-
-		cv::waitKey(0);
-		cv::destroyAllWindows();
+		if (is_interactive){
+			cv::imshow(inputfile+" [original]", src);
+			cv::imshow(outputfile+" [result]", dst);
+			cv::waitKey(0);
+			cv::destroyAllWindows();
+		}
+		else{
+			cv::imwrite(outputfile, dst);
+		}
 	}
 
 	return 0;
